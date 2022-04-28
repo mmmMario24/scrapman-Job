082f463e34fb21ee279bc0fc97eb11c0d3e67541
mmmMario <74585097+mmmMario24@users.noreply.github.com>
2022-04-28 13:53:18 +0300

    Add files via upload

---
+++ scrapman-job/main/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+-- set whichever framework you prefer to true
+Config.useESX = false -- If you use ESX change to true
+Config.useQBCore = false -- If you use QBCore change to true
+Config.useVRP = true -- If you use vRP change to true
